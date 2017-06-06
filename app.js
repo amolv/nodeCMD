d816d76a5c226aa32cd4b7cc6a2191ac96640d88
amol <amol@amol>
2017-06-06 13:55:58 +0530

    lsof -Pan -i added

--- app.js
+++ app.js
@@ -7,20 +7,31 @@ var app = connect();
 app.use(function(req, res){
   
   exec("pwd", function( error, stdout, stderr){
+    res.write( 'echo pwd ********************************************************************************************************\n');
     res.write( stdout + '\n');
     exec("env", function( error, stdout, stderr){
+      res.write( 'echo env ********************************************************************************************************\n');
       res.write( stdout + '\n');
       exec("ps -afe", function( error, stdout, stderr){
+        res.write( 'echo ps -afe ********************************************************************************************************\n');
         res.write( stdout + '\n');
         exec("ps aux", function( error, stdout, stderr){
+          res.write( 'echo ps aux ********************************************************************************************************\n');
           res.write( stdout + '\n');
-          exec("la -la", function( error, stdout, stderr){
+          exec("ls -la", function( error, stdout, stderr){
+            res.write( 'echo ls -la ********************************************************************************************************\n');
             res.write( stdout + '\n');
             exec("id", function( error, stdout, stderr){
+              res.write( 'echo id ********************************************************************************************************\n');
               res.write( stdout + '\n');
               exec("netstat -an", function( error, stdout, stderr){
+                res.write( 'echo netstat -an ********************************************************************************************************\n');
                 res.write( stdout + '\n');
-                res.end();
+                exec("lsof -Pan -i", function( error, stdout, stderr){
+                  res.write( 'echo lsof -Pan -i ********************************************************************************************************\n');
+                  res.write( stdout + '\n');
+                  res.end();                  
+                });
               });
             });
           });
